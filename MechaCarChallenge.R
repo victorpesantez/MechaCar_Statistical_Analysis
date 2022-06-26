ec1ea0be32cc762dd3e899281ed8725ba9855e3c
unknown <victorpesantez_07@hotmail.com>
2022-06-26 15:40:34 -0400

    Updated Challenge 15

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -3,7 +3,7 @@ MechaCar_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactor
 predicts_mpg <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD ,MechaCar_table) 
 summary(predicts_mpg)
 Suspension_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
-summarize_suspension <- Suspension_table %>% summarize(Mean=mean(PSI), Median=median(PSI),SD=sd(PSI))
+summarize_suspension <- Suspension_table %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI),SD=sd(PSI))
 summarize_suspension 
 lot_summary <- Suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
 lot_summary
